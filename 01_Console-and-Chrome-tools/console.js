f911800467348c2701106a59c618098693012b64
TechiAkki963 <paraba777@gmail.com>
2023-11-14 19:55:00 +0530

    00-js-in-depth

---
+++ 01_Console-and-Chrome-tools/console.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("Hello World");
+
+alert("hello world");   // for popup 
+
+
+prompt("Enter a Comment");  //is use for user input popup
+console.log(prompt("enter a comment")); //if want to log in console
