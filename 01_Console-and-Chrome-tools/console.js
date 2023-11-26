865d7c237de0bda1c305fdf059cd7facc6392c1e
TechiAkki963 <paraba777@gmail.com>
2023-11-26 19:14:50 +0530

    console done

--- 01_Console-and-Chrome-tools/console.js
+++ 01_Console-and-Chrome-tools/console.js
@@ -2,7 +2,13 @@
 
 console.log("Hello World");
 
+console.table({ name: "Akshay", Email: "akshay@xyz.com" });
+
 //? alert("hello world");                             // for popup
 
 //? prompt("Enter a Comment");                        //is use for user input popup
 //? console.log(prompt("enter a comment"));          //if want to log in console
+
+console.error("Alert"); //! to display error in console
+
+console.warn("Warning"); //**to display warning */
