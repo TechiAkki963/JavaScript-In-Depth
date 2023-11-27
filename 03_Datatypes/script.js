990c4a1bff7dad4aeab57982da921ea3e61bd232
TechiAkki963 <paraba777@gmail.com>
2023-11-27 13:28:29 +0530

    Primitive Datatypes

--- 03_Datatypes/script.js
+++ 03_Datatypes/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+//? Primitive Datatype
+
+//? String
+const firstName = "Sarah";
+console.log(firstName, typeof firstName); //Sarah String
+
+//? Number
+const num = 369;
+const temp = 98.9;
+console.log(num, typeof num); //369 number
+console.log(temp, typeof temp); //98.9 number
+
+//? Boolean
+const canSwim = true;
+console.log(canSwim, typeof canSwim); //true boolean
+
+//? Null
+//*It is intentionally empty value
+const optNumbers = null; //null object
+console.log(optNumbers, typeof optNumbers);
+
+//? Undefined
+let score;
+console.log(score, typeof score); //undefined undefined
+
+//? Symbol
+const id = Symbol("id");
+console.log(id, typeof id); //Symbol(id) symbol
